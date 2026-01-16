a74b1b7a2243902b5e633b8c29ca8bc7b7ca5366
JonaMG0812 <join_mg@hotmail.com>
2026-01-16 17:59:23 -0600

    enable video recording, screenshots on run failure test and update gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.saucedemo.com"
+    baseUrl: "https://www.saucedemo.com",
+    video: true,
+    screenshotOnRunFailure: true,
   },
 });
